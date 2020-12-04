1a49fa9703a90f2fff85ae160269be8404ae0150
annejenkins-cgi <73593442+annejenkins-cgi@users.noreply.github.com>
2020-12-04 16:21:19 +0000

    Update server.js

--- server.js
+++ server.js
@@ -107,6 +107,7 @@ function handleTwilioMessages(sessionHandler) {
 function sendTwilioMessage(teneoResponse, res, triggerFrom) {
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 if(triggerFrom!==undefined && triggerFrom!==null && triggerFrom!="") {
+    console.log('trying to send outbound message');
 client.messages
       .create({
          from: TWILIO_OUTBOUND_NUMBER,
